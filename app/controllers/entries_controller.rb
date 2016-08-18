094ce12bb64189eae43531ab37c03af062f5714d
nosewicz <joshnosewicz@gmail.com>
2016-08-18 03:36:17 +0000

    added layout

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -3,7 +3,7 @@ class EntriesController < ApplicationController
 
 
   def index
-    @entries = Entry.all.order('published desc')
+    @entries = Entry.all.order('published desc').limit(50)
    @buzzposts = Entry.where(:feed_id => 1).order('published desc').limit(10)
    @thoughts = Entry.where(:feed_id => 2).order('published desc').limit(10)
    @chives = Entry.where(:feed_id => 3).order('published desc').limit(10)
